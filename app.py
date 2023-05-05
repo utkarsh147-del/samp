02d2954ef6260c34a2aa0d7c2ef67064bdeb64aa
Utkarsh Chauhan <73778569+utkarsh147-del@users.noreply.github.com>
2023-05-05 15:11:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,31 +15,32 @@ class StunVideoProcessor(VideoProcessorBase):
 
     async def setup(self):
         # Configure STUN and TURN servers
-        configuration = aiortc.RTCConfiguration(
-            iceServers=[{
-   urls: [ "stun:bn-turn1.xirsys.com" ]
-}, {
-   username: "TBPiEMw7tX24LLZvFW8ymeB-DwRFYTzQ8eVh1B3yroLEeERJ4lBh7HQKQXXD6gJaAAAAAGRUyXJ1dGthcnNoMzU2",
-   credential: "87bac70a-eb25-11ed-b31b-0242ac140004",
-   urls: [
-       "turn:bn-turn1.xirsys.com:80?transport=udp",
-       "turn:bn-turn1.xirsys.com:3478?transport=udp",
-       "turn:bn-turn1.xirsys.com:80?transport=tcp",
-       "turn:bn-turn1.xirsys.com:3478?transport=tcp",
-       "turns:bn-turn1.xirsys.com:443?transport=tcp",
-       "turns:bn-turn1.xirsys.com:5349?transport=tcp"
-   ]
-}]
-        )
+        
+        ice_servers=[
+         {"urls": "stun:bn-turn1.xirsys.com"},
+            {
+            "urls": [
+                "turn:bn-turn1.xirsys.com:80?transport=udp",
+                "turn:bn-turn1.xirsys.com:3478?transport=udp",
+                "turn:bn-turn1.xirsys.com:80?transport=tcp",
+                "turn:bn-turn1.xirsys.com:3478?transport=tcp",
+                "turns:bn-turn1.xirsys.com:443?transport=tcp",
+                "turns:bn-turn1.xirsys.com:5349?transport=tcp"
+            ],
+            "username": "TBPiEMw7tX24LLZvFW8ymeB-DwRFYTzQ8eVh1B3yroLEeERJ4lBh7HQKQXXD6gJaAAAAAGRUyXJ1dGthcnNoMzU2",
+            "credential": "87bac70a-eb25-11ed-b31b-0242ac140004
+        }
+    ]
+        configuration = aiortc.RTCConfiguration(iceServers=ice_servers)
 
-        # Create WebRTC connection
+    # Create WebRTC connection
         self.webrtc_ctx = self._create_webrtc_context(
-            configuration=configuration
-        )
+        configuration=configuration
+    )
 
 # Run the WebRTC streamer
 webrtc_streamer(
-    key="example",
+    key="eutkarsh",
     mode=WebRtcMode.SENDRECV,
     video_processor_factory=StunVideoProcessor,
 )
