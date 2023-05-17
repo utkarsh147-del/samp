f3d76a9ad94e94fc276739c4bfcf6fd4af83f33c
Utkarsh Chauhan <73778569+utkarsh147-del@users.noreply.github.com>
2023-05-18 00:05:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,27 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, RTCConfiguration, VideoProcessorBase
+from streamlit.components.v1 import html
 
-# Set up the RTCConfiguration with your TURN server details
-rtc_configuration = RTCConfiguration(
-    {
-        "iceServers": [
-            {
-                "urls": ["turn:bn-turn1.xirsys.com:80?transport=udp",
-       "turn:bn-turn1.xirsys.com:3478?transport=udp",
-       "turn:bn-turn1.xirsys.com:80?transport=tcp",
-       "turn:bn-turn1.xirsys.com:3478?transport=tcp",
-       "turns:bn-turn1.xirsys.com:443?transport=tcp",
-       "turns:bn-turn1.xirsys.com:5349?transport=tcp"],
-                "username": "TBPiEMw7tX24LLZvFW8ymeB-DwRFYTzQ8eVh1B3yroLEeERJ4lBh7HQKQXXD6gJaAAAAAGRUyXJ1dGthcnNoMzU2",
-                "credential": "87bac70a-eb25-11ed-b31b-0242ac140004",
-            }
-        ]
-    }
-)
+html_string = """
+<video id="camera-stream" width="640" height="480" autoplay></video>
+<button id="take-photo">Take Photo</button>
 
-# Create a custom VideoProcessor class to use with the WebRTC component
-class VideoProcessor(VideoProcessorBase):
-    pass
+<script>
+  const video = document.querySelector('#camera-stream');
+  const takePhotoButton = document.querySelector('#take-photo');
 
-# Use the webrtc_streamer function with the custom rtc_configuration and VideoProcessor
-webrtc_streamer(
-    key="example",
-    rtc_configuration=rtc_configuration,
-    video_processor_factory=VideoProcessor,
-)
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(stream => {
+      video.srcObject = stream;
+    })
+    .catch(error => {
+      console.error('Error accessing camera', error);
+    });
+
+  takePhotoButton.addEventListener('click', () => {
+    // code to take a photo
+  });
+</script>
+"""
+
+html(html_string, width=640, height=520)
